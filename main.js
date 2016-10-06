8ef657ba174dfbcf929543c190e5b602202121e6
Ryan Lee <RyanLee@Ryans-MacBook-Pro.local>
2016-10-05 23:20:28 -0700

    final commit

--- main.js
+++ main.js
@@ -5,15 +5,63 @@
 	    
 	})
 
-var avg = price / items.length
+       avg = price / items.length
 
-avg.toFixed(2)
+console.log(avg.toFixed(2))
 
+
+document.getElementById('answer1').innerHTML = avg.toFixed(2)
+
+var arr = []
 var newArr = items.filter(function(item){
-	 
-  if (items.price > 14 && items.price < 18) {
-    return true
-  }else{
-  	return false
+  if (item.price > 14 && item.price < 18) {
+    arr += item.title
   }
 })
+console.log(arr)
+document.getElementById('answer2').innerHTML = arr
+
+var GBP = ""
+var currency = items.filter(function(item){
+	if (item.currency_code === "GBP") {
+      GBP += item.title + " " + item.price
+		 
+	}
+	
+})
+console.log(GBP)
+document.getElementById('answer3').innerHTML = GBP
+
+var woodItems = []
+var newArr = items.filter(function(item){
+	if(item.materials.indexOf("wood") !== -1){
+		woodItems += item.title
+	}
+})
+console.log(woodItems)
+
+document.getElementById('answer4').innerHTML = woodItems
+
+var morMaterials = []
+var newArr = items.filter(function(item){
+	if (item.materials.length >= 8 ) {
+		 morMaterials += item.title +" "+ item.materials + " " + item.materials.length
+}
+})
+console.log(morMaterials)
+
+
+document.getElementById('answer5').innerHTML = morMaterials
+
+var me = 0
+var numb = items.forEach(function(item){
+	me += item.who_made === "i_did" 
+		
+})
+newNum = me.length 
+console.log(me + " " + "were made by sellers")
+
+document.getElementById('answer6').innerHTML = me + " " + "were made by sellers"
+
+
+
